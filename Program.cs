c8a2554cd0e09dbc15ba2a222169a13731fd9820
Giorgi Giorgobiani <129969647+GiorgiGiorgobianni@users.noreply.github.com>
2024-06-26 01:19:06 +0400

    add GamestoreContext builder to resolve source path error

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using GameStore.api.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = "Date Source=GameStore.db";
+builder.Services.AddSqlite<GamestoreContext>(connectionString);
+
 
 var app = builder.Build();
 
